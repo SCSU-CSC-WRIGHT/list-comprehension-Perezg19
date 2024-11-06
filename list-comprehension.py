49f5ae57c1dc5ca38f60d1cec83dcf261db70d54
Perezg19 <gabrielperez.83003@gmail.com>
2024-11-06 21:39:35 +0000

    Lab Unfinished

---
+++ list-comprehension.py
@@ -0,0 +1,28 @@
+def lab_1():
+    num=int(input("Enter the number of scores: "))
+    scores=[]
+    for i in range(num):
+        score=int(input("Enter the score "))
+        scores.append (score)
+        sum(scores)/len(scores)
+avg = sum(scores)/len(scores)
+if avg>= 90:
+    print("A")
+elif avg>= 80 and avg<= 90:
+    print("B")
+elif avg>= 70 and avg<= 80:
+    print("C")
+elif avg>= 60 and avg<= 70:
+    print("D")
+elif avg<= 59:
+    print("F")
+lab_1
+#avg=sum(scores)/len(scores) (Do this outside of the for loop)
+    #if avg>=90:
+        #print("A")
+    #elif avg>= 80 and 90:
+        #print("B")
+# print(scores)
+# invoke function with lab_1
+
+def lab_2(): split.()
