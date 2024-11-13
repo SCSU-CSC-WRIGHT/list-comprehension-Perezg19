90b9b898712de9f1ea8fdac8e68972993cad1497
Perezg19 <gabrielperez.83003@gmail.com>
2024-11-13 21:37:10 +0000

    Unfinished Lab

--- list-comprehension.py
+++ list-comprehension.py
@@ -5,24 +5,39 @@ def lab_1():
         score=int(input("Enter the score "))
         scores.append (score)
         sum(scores)/len(scores)
-avg = sum(scores)/len(scores)
-if avg>= 90:
-    print("A")
-elif avg>= 80 and avg<= 90:
-    print("B")
-elif avg>= 70 and avg<= 80:
-    print("C")
-elif avg>= 60 and avg<= 70:
-    print("D")
-elif avg<= 59:
-    print("F")
-lab_1
-#avg=sum(scores)/len(scores) (Do this outside of the for loop)
-    #if avg>=90:
-        #print("A")
-    #elif avg>= 80 and 90:
-        #print("B")
-# print(scores)
-# invoke function with lab_1
+    avg = sum(scores)/len(scores)
+    if avg>= 90:
+        print("A")
+    elif avg>= 80 and avg<= 90:
+        print("B")
+    elif avg>= 70 and avg<= 80:
+        print("C")
+    elif avg>= 60 and avg<= 70:
+        print("D")
+    elif avg<= 59:
+        print("F")
+    avg=sum(scores)/len(scores) #(Do this outside of the for loop)
+    if avg>=90:
+        print("A")
+    elif avg>= 80 and 90:
+        print("B")
+    print(scores)
+lab_1()
 
-def lab_2(): split.()
+
+#invoke function with lab_1
+
+#def lab_2(): split.()
+
+#def lab_3(): 
+#numbers = [1,7,11,13]
+#test = [1,2,4,7]
+    #for num in numbers:
+        #if num in test:
+        #test.remove(num)
+    #return numbers + test
+    #check=input
+    #if check=exit
+        #break
+    #Skip second bullet
+    #Skip last bullet 
