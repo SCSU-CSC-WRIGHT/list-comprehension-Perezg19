7c51a6e47a782502d0972ae0980bffde60e34315
Perezg19 <gabrielperez.83003@gmail.com>
2024-11-14 03:49:48 +0000

    Unfinished Lab

--- list-comprehension.py
+++ list-comprehension.py
@@ -22,7 +22,18 @@ def lab_1():
     elif avg>= 80 and 90:
         print("B")
     print(scores)
-lab_1()
+#lab_1()
 
 def lab_2():
-    
+    numask=int(input("How many numbers will you add? "))
+    oddl=[]
+    evenl=[]
+    for i in range(numask):
+        intask=int(input("Please enter various numbers: "))
+        if i % 2 != 0:
+            oddl.append (intask)
+        if i % 2 == 0:
+            evenl.append (intask)
+        print(oddl)
+        print(evenl)
+lab_2()
