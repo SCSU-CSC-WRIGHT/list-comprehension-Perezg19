ce29ac5c16dad7381ed6280e80ce3aa17adcff8f
Perezg19 <gabrielperez.83003@gmail.com>
2024-11-20 01:47:49 +0000

    LABS ALL FINISHED

--- list-comprehension.py
+++ list-comprehension.py
@@ -30,26 +30,30 @@ def lab_2():
     evenl=[]
     for i in range(num_of_int):
         int_input=(input(f"Please enter your numbers for your lists, and seperate with spaces: "))
-        numbers = int_input.split()
-        for nums in numbers:
-            num=int(nums)
-        if num % 2 == 0:
-            oddl.append (num)
-        else:
-            evenl.append (num)
+        for i in int_input.split():
+            i=(int(i))
+            if i % 2 == 0:
+                oddl.append (i)
+            else:
+                evenl.append (i)
         print("\nOdd numbers: ", oddl)
         print("Even numbers: ", evenl)
 #lab_2()
 
 def lab_3():
-    list_1=[]
-    list_2=[]
-    list_3=list_1+list_2
+    list_1= []
+    list_2= []
     ask_input1=(input("Please enter numbers for list 1, seperated with spaces: "))
-    for i in ask_input1:
-        list_1.append(i)
+    for i in ask_input1.split():
+        list_1.append(int(i))
     ask_input2=(input("Please enter numbers for list 2, seperated with spaces: "))
-    for i in ask_input2:
-        list_2.append(i)
-    print(list_1, list_2)
+    for i in ask_input2.split():
+        list_2.append(int(i))
+    print("Here are your lists:",list_1, list_2)
+    print("Now watch as I remove all your duplicates, ")
+    list_3=list_1+list_2
+    for number in list_1:
+        if number in list_2:
+            list_3.remove(number)
+    print("Here is your merged list with no duplicate numbers! ", list_3)
 lab_3()
